898c035ce714d8200a23c31b62eed385c6cd192d
Chaira Joel <chaira.cynthia.joel@gmail.com>
2023-10-26 21:43:54 +0200

    changes

---
+++ tests/addPersonalInformation.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("Should be able to add personal information", async ({ page }) => {
+  await page.goto("https://techblog.polteq.com/testshop/index.php");
+  await page.locator('//*[@id="header"]/div[1]/div/div/nav/div[2]/a').click();
+  await page
+    .locator('//*[@id="center_column"]/div/div[1]/ul/li[4]/a/span')
+    .click();
+
+  await page.locator("#id_gender2").check();
+  expect(await page.locator("#id_gender2").isChecked()).toBeTruthy();
+});
--- tests/buylaptop.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("Should be able to add address details", async ({ page }) => {
-  await page.goto("https://techblog.polteq.com/testshop/index.php");
-  await expect(page.getByRole("link", { name: "Sign out" })).toBeVisible();
-});
